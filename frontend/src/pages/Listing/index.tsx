4f4661ce351bdf5fa318953ac8f9c54f86db25fd
Flavio Rogerio <solebac@hotmail.com>
2022-03-11 23:08:04 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,14 +1,21 @@
+import axios from "axios";
 import { Fragment } from "react";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+
+  axios.get(`${BASE_URL}/movies?size=2&page=2`)
+  .then(response => {
+    console.log(response.data); 
+  })
   return (
     <Fragment>
       <Pagination />
       <div className="container">
         <div className="row">
-        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
---
+++ frontend/src/utils/requests.ts
@@ -0,0 +1,4 @@
+//Definir BASE_URL
+//process.env = Busca variaveis de ambiente(propriedades do project) do React
+//REACT_APP_BACKEND_URL
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
